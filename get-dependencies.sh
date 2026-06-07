f27d3309b5242c7105df89656b28f04e58195d88
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-07 13:07:07 -0400

    mmm

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,7 +16,7 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-#get-debloated-pkgs --add-common --prefer-nano x265-mini
+get-debloated-pkgs --add-mesa --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
